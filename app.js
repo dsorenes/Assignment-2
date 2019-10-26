b8393b6f7bf4a4c88d1c3133c4ca62c073916680
Daniel Sørenes <danielsorenes@hotmail.no>
2019-10-26 22:17:16 +1000

    fixed indentation error

--- app.js
+++ app.js
@@ -25,151 +25,151 @@ const access_secret = process.env.ACCESS_SECRET;
 
 
 oa = new OAuth(
-	"https://api.twitter.com/oauth/request_token",
-	"https://api.twitter.com/oauth/access_token",
-	consumer_key,
-	consumer_secret,
-	"1.0",
-	"",
-	"HMAC-SHA1"
+  "https://api.twitter.com/oauth/request_token",
+  "https://api.twitter.com/oauth/access_token",
+  consumer_key,
+  consumer_secret,
+  "1.0",
+  "",
+  "HMAC-SHA1"
 );
 
 const bucketName = "casd-test-storage";
 
 new AWS.S3({ apiVersion: "2006-03-01" })
-	.createBucket({ Bucket: bucketName })
-	.promise()
-	.then(data => console.log("created: ", bucketName))
-	.catch(error => console.log(error, error.stack));
+  .createBucket({ Bucket: bucketName })
+  .promise()
+  .then(data => console.log("created: ", bucketName))
+  .catch(error => console.log(error, error.stack));
 
 app.get("/get/tweets", async (req, res) => {
-	let amount_of_tweets = req.query.amount;
+  let amount_of_tweets = req.query.amount;
 
-	let hashtagQuery = encodeURIComponent(req.query.q);
+  let hashtagQuery = encodeURIComponent(req.query.q);
 
-	const redisKey = `hashtags:${hashtagQuery}`;
-	const bucketKey = `hashtags-${hashtagQuery}`;
+  const redisKey = `hashtags:${hashtagQuery}`;
+  const bucketKey = `hashtags-${hashtagQuery}`;
 
-	const query = `?max_id=${req.query.max_id}&since_id=${req.query.min_id}&q=${hashtagQuery}&include_entities=1&count=100`;
+  const query = `?max_id=${req.query.max_id}&since_id=${req.query.min_id}&q=${hashtagQuery}&include_entities=1&count=100`;
 
-	return redisClient.get(redisKey, (error, result) => {
-		if (error) console.log(error);
+  return redisClient.get(redisKey, (error, result) => {
+    if (error) console.log(error);
 
-		if (result) {
-			const tweetAnalysisFromCache = JSON.parse(result);
+    if (result) {
+      const tweetAnalysisFromCache = JSON.parse(result);
 
-			//If amount from cache is less than user specified, fetch remaining data from Twitter and store that in cache too
-			if (amount_of_tweets > tweetAnalysisFromCache.amount_of_tweets) {
-				let amount = amount_of_tweets - tweetAnalysisFromCache.amount_of_tweets;
-				getTweets(query, amount, null, tweets => {
-					let tweetAnalysisFromAPI = analyseTweets(tweets);
+      //If amount from cache is less than user specified, fetch remaining data from Twitter and store that in cache too
+      if (amount_of_tweets > tweetAnalysisFromCache.amount_of_tweets) {
+        let amount = amount_of_tweets - tweetAnalysisFromCache.amount_of_tweets;
+        getTweets(query, amount, null, tweets => {
+          let tweetAnalysisFromAPI = analyseTweets(tweets);
 
-					tweetAnalysisFromCache.amount_of_tweets +=
-						tweetAnalysisFromAPI.amount_of_tweets;
-					tweetAnalysisFromCache.most_important_words.push(
-						...tweetAnalysisFromAPI.most_important_words
-					);
-					tweetAnalysisFromCache.sentimentAnalysis_per_tweet.push(
-						...tweetAnalysisFromAPI.sentimentAnalysis_per_tweet
-					);
-					tweetAnalysisFromCache.hashtagCount.push(
-						...tweetAnalysisFromAPI.hashtagCount
-					);
+          tweetAnalysisFromCache.amount_of_tweets +=
+            tweetAnalysisFromAPI.amount_of_tweets;
+          tweetAnalysisFromCache.most_important_words.push(
+            ...tweetAnalysisFromAPI.most_important_words
+          );
+          tweetAnalysisFromCache.sentimentAnalysis_per_tweet.push(
+            ...tweetAnalysisFromAPI.sentimentAnalysis_per_tweet
+          );
+          tweetAnalysisFromCache.hashtagCount.push(
+            ...tweetAnalysisFromAPI.hashtagCount
+          );
 
-					redisClient.setex(
-						redisKey,
-						3600,
-						JSON.stringify(tweetAnalysisFromCache)
-					);
+          redisClient.setex(
+            redisKey,
+            3600,
+            JSON.stringify(tweetAnalysisFromCache)
+          );
 
-					return res.status(200).send({
-						source: "Redis cache and Twitter API",
-						tweetAnalysisFromCache
-					});
-				});
-			} else {
-				return res.status(200).send({ source: "Redis cache", tweetAnalysisFromCache });
-			}
-		} else {
-			//check if it's in S3
-			//need to do the same here as with the cache. If the total amount is larger than what already is in S3, fetch from API as well and update S3 content with new amount
-			const params = { Bucket: bucketName, Key: bucketKey };
+          return res.status(200).send({
+            source: "Redis cache and Twitter API",
+            tweetAnalysisFromCache
+          });
+        });
+      } else {
+        return res.status(200).send({ source: "Redis cache", tweetAnalysisFromCache });
+      }
+    } else {
+      //check if it's in S3
+      //need to do the same here as with the cache. If the total amount is larger than what already is in S3, fetch from API as well and update S3 content with new amount
+      const params = { Bucket: bucketName, Key: bucketKey };
 
-			return new AWS.S3({ apiVersion: "2006-03-1" })
-				.getObject(params, (error, result) => {
-					if (result) {
-						let data = result.Body.toString("utf-8");
+      return new AWS.S3({ apiVersion: "2006-03-1" })
+        .getObject(params, (error, result) => {
+          if (result) {
+            let data = result.Body.toString("utf-8");
 
-						let tweetAnalysisFromS3 = JSON.parse(data);
+            let tweetAnalysisFromS3 = JSON.parse(data);
 
-						if (amount_of_tweets > tweetAnalysisFromS3.amount_of_tweets) {
-							let amount = amount_of_tweets - tweetAnalysisFromS3.amount_of_tweets;
-							getTweets(query, amount, null, tweets => {
-								let tweetAnalysisFromAPI = analyseTweets(tweets);
-			
-								tweetAnalysisFromS3.amount_of_tweets +=
-									tweetAnalysisFromAPI.amount_of_tweets;
-								tweetAnalysisFromS3.most_important_words.push(
-									...tweetAnalysisFromAPI.most_important_words
-								);
-								tweetAnalysisFromS3.sentimentAnalysis_per_tweet.push(
-									...tweetAnalysisFromAPI.sentimentAnalysis_per_tweet
-								);
-								tweetAnalysisFromS3.hashtagCount.push(
-									...tweetAnalysisFromAPI.hashtagCount
-								);
-								redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysisFromS3));
+            if (amount_of_tweets > tweetAnalysisFromS3.amount_of_tweets) {
+              let amount = amount_of_tweets - tweetAnalysisFromS3.amount_of_tweets;
+              getTweets(query, amount, null, tweets => {
+                let tweetAnalysisFromAPI = analyseTweets(tweets);
+      
+                tweetAnalysisFromS3.amount_of_tweets +=
+                  tweetAnalysisFromAPI.amount_of_tweets;
+                tweetAnalysisFromS3.most_important_words.push(
+                  ...tweetAnalysisFromAPI.most_important_words
+                );
+                tweetAnalysisFromS3.sentimentAnalysis_per_tweet.push(
+                  ...tweetAnalysisFromAPI.sentimentAnalysis_per_tweet
+                );
+                tweetAnalysisFromS3.hashtagCount.push(
+                  ...tweetAnalysisFromAPI.hashtagCount
+                );
+                redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysisFromS3));
 
-								new AWS.S3({ apiVersion: "2006-03-01" })
-								.putObject({
-									Bucket: bucketName,
-									Key: bucketKey,
-									Body: JSON.stringify(tweetAnalysisFromS3)
-								})
-								.promise()
-								.then(data =>
-									console.log(
-										"successfully uploaded data to ",
-										bucketName,
-										bucketKey
-									)
-								);
+                new AWS.S3({ apiVersion: "2006-03-01" })
+                .putObject({
+                  Bucket: bucketName,
+                  Key: bucketKey,
+                  Body: JSON.stringify(tweetAnalysisFromS3)
+                })
+                .promise()
+                .then(data =>
+                  console.log(
+                    "successfully uploaded data to ",
+                    bucketName,
+                    bucketKey
+                  )
+                );
 
-								return res.status(200).send({ source: "S3 storage and Twitter API", tweetAnalysisFromS3 });
-								});
-							} else {
-								redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysisFromS3));
-								return res.status(200).send({ source: "S3 storage", tweetAnalysisFromS3 }); 
-							}
-					} else {
-						//It's not in S3. Pull from Twitter API and push the analysed data into both S3 and the Redis Cache
-						getTweets(query, amount_of_tweets, null, tweets => {
-							let tweetAnalysis = analyseTweets(tweets);
+                return res.status(200).send({ source: "S3 storage and Twitter API", tweetAnalysisFromS3 });
+                });
+              } else {
+                redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysisFromS3));
+                return res.status(200).send({ source: "S3 storage", tweetAnalysisFromS3 }); 
+              }
+          } else {
+            //It's not in S3. Pull from Twitter API and push the analysed data into both S3 and the Redis Cache
+            getTweets(query, amount_of_tweets, null, tweets => {
+              let tweetAnalysis = analyseTweets(tweets);
 
-							redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysis));
+              redisClient.setex(redisKey, 3600, JSON.stringify(tweetAnalysis));
 
-							new AWS.S3({ apiVersion: "2006-03-01" })
-								.putObject({
-									Bucket: bucketName,
-									Key: bucketKey,
-									Body: JSON.stringify(tweetAnalysis)
-								})
-								.promise()
-								.then(data =>
-									console.log(
-										"successfully uploaded data to ",
-										bucketName,
-										bucketKey
-									)
-								);
+              new AWS.S3({ apiVersion: "2006-03-01" })
+                .putObject({
+                  Bucket: bucketName,
+                  Key: bucketKey,
+                  Body: JSON.stringify(tweetAnalysis)
+                })
+                .promise()
+                .then(data =>
+                  console.log(
+                    "successfully uploaded data to ",
+                    bucketName,
+                    bucketKey
+                  )
+                );
 
-							return res.status(200).send({ source: "Twitter API", tweetAnalysis });
-						});
-					}
-				}
-			);
-		}
-	});
+              return res.status(200).send({ source: "Twitter API", tweetAnalysis });
+            });
+          }
+        }
+      );
+    }
+  });
 });
 /**
  * @param {string}   query            the hashtags to use as query for the Twitter API search.
@@ -178,60 +178,60 @@ app.get("/get/tweets", async (req, res) => {
  * @param {callback} callback         the callback function to supplement the data with.
  */
 let getTweets = async (
-	query,
-	amount_of_tweets = 500,
-	data = null,
-	callback
+  query,
+  amount_of_tweets = 500,
+  data = null,
+  callback
 ) => {
-	let old_data = data;
+  let old_data = data;
 
-	try {
-		oa.get(
-			`https://api.twitter.com/1.1/search/tweets.json${query}&lang=en`,
-			access_token,
-			access_secret,
-			(error, data, response) => {
-				if (error) {
-					console.log(error);
-				}
+  try {
+    oa.get(
+      `https://api.twitter.com/1.1/search/tweets.json${query}&lang=en`,
+      access_token,
+      access_secret,
+      (error, data, response) => {
+        if (error) {
+          console.log(error);
+        }
 
-				if (data === [] || data === undefined) {
-					getTweets(query, amount_of_tweets, null, callback);
-				}
+        if (data === [] || data === undefined) {
+          getTweets(query, amount_of_tweets, null, callback);
+        }
 
-				let tweets = JSON.parse(data);
+        let tweets = JSON.parse(data);
 
-				let parsed_tweets = utils.parseTweets(tweets);
-				if (old_data !== null) {
-					parsed_tweets.statuses = old_data.statuses.concat(
-						parsed_tweets.statuses
-					);
-				}
+        let parsed_tweets = utils.parseTweets(tweets);
+        if (old_data !== null) {
+          parsed_tweets.statuses = old_data.statuses.concat(
+            parsed_tweets.statuses
+          );
+        }
 
-				let total = parsed_tweets.statuses.length;
-				console.log(total);
+        let total = parsed_tweets.statuses.length;
+        console.log(total);
 
-				let difference = Math.abs(amount_of_tweets - total);
+        let difference = Math.abs(amount_of_tweets - total);
 
-				if (total < amount_of_tweets) {
-					let count = difference > 100 ? 100 : difference + 100;
+        if (total < amount_of_tweets) {
+          let count = difference > 100 ? 100 : difference + 100;
 
-					let min_id = parsed_tweets.search_metadata.min_id;
-					let max_id = parsed_tweets.search_metadata.maximum_id;
+          let min_id = parsed_tweets.search_metadata.min_id;
+          let max_id = parsed_tweets.search_metadata.maximum_id;
 
-					let old_query = parsed_tweets.search_metadata.query;
+          let old_query = parsed_tweets.search_metadata.query;
 
-					let new_query = `?max_id=${max_id}&since_id=${min_id}&q=${old_query}&include_entities=1&count=${count}`;
+          let new_query = `?max_id=${max_id}&since_id=${min_id}&q=${old_query}&include_entities=1&count=${count}`;
 
-					getTweets(new_query, amount_of_tweets, parsed_tweets, callback);
-				} else {
-					callback(parsed_tweets);
-				}
-			}
-		);
-	} catch (e) {
-		console.log(e);
-	}
+          getTweets(new_query, amount_of_tweets, parsed_tweets, callback);
+        } else {
+          callback(parsed_tweets);
+        }
+      }
+    );
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 console.log("listening on port 8080");
